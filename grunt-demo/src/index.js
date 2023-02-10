eba981ca71ac060c68af3a9f058501d0df9a7abb
lifuhai <fuhai.li@gientech.com>
2023-02-10 12:34:24 +0800

    grunt-demo

---
+++ grunt-demo/src/index.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    uglify: {
+      options: {
+        compress: true,
+      },
+      build: {
+        src: "src/index.js",
+        dest: "build/index.min.js",
+      },
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+
+  grunt.registerTask("default", ["uglify"]);
+};
